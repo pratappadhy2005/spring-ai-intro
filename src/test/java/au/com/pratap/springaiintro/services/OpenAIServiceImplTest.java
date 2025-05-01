3914b9c82244c07c6b6acfd392e2bb92bed285d1
Pratap Padhy <pratappadhy2005@gmail.com>
2025-05-01 20:23:04 +1000

    Added open AI service capability

---
+++ src/test/java/au/com/pratap/springaiintro/services/OpenAIServiceImplTest.java
@@ -0,0 +1,22 @@
+package au.com.pratap.springaiintro.services;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+class OpenAIServiceImplTest {
+
+    @Autowired
+    OpenAIService openAIService;
+
+    @Test
+    void getAnswer() {
+
+        String answer = openAIService.getAnswer("Tell me a dad joke.");
+        System.out.println("Got the answer");
+        System.out.println(answer);
+    }
+}
